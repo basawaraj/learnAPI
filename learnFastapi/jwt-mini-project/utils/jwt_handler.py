ac548429693ea0b4fc419778b8ebdcb00c618213
basu <basawarajmulawad@gmail.com>
2026-05-14 23:58:57 +0530

    learning jwt auth

---
+++ learnFastapi/jwt-mini-project/utils/jwt_handler.py
@@ -0,0 +1,14 @@
+from jose import JWTError, jwt
+from datetime import datetime, timedelta
+
+SECRET_KEY="mysecretkey"
+ALGORITHM="HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES=2
+
+def create_access_token (data:dict):
+    to_encode=data.copy()
+    expire=datetime.utcnow()+ timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+
+    token_jwt=jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return token_jwt
